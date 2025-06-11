c94ba348d92b6f4f326760d4a22f48894aa59c53
marcianobeat <marcianobeat7@gmail.com>
2025-06-11 01:55:51 +0000

    teste 1

--- xadrez.c
+++ xadrez.c
@@ -3,27 +3,45 @@ int main(){
     int torre = 0;
     int bispo = 0;
     int rainha = 0;
+    int cavalo = 1;
 
-printf("Torre\n");
+printf("Torre:\n");
 
-    for (torre = 0; torre < 5; torre++) {
+    for (torre; torre < 5; torre++) {
         printf ("direita\n");
     }
+printf("\n");
 
-printf("Rainha\n");
+printf("Rainha:\n");
 
     while (rainha <= 8){
     printf ("esquerda\n");
     rainha++;
     }
+printf("\n");
 
-printf("Bispo\n");
+printf("Bispo:\n");
     do { 
-        printf("esquerda para cima\n");
+        printf("esquerda\n");
+        printf("cima\n");
         bispo++;
 
     } while (bispo <=5);
+
+printf("\n");
     
+printf("Cavalo:\n");
+    while (cavalo--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Cima\n");
+        }
+    printf("Esquerda\n");    
+        
+    }
+    
+
     
     return 0;
 }
